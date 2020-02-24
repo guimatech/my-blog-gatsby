74c91a9c94d1148c3b4252633e07e7ebc96303cd
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-02-24 16:49:39 -0300

    - Add SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,28 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/guimaraes-lucas'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/AboutGuimaraes'
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/lucas-dos-santos-guima/'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/guimaraes2199/'
+  },
+  {
+    label: 'Pinterest',
+    url: 'https://br.pinterest.com/lucasasafe/'
+  },
+  {
+    label: 'Facebook',
+    url: 'https://www.facebook.com/lucas.s.guimaraes.09?ref=bookmarks'
+  }
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,17 @@
+import { Github } from 'styled-icons/boxicons-logos/Github'
+import { Twitter } from 'styled-icons/boxicons-logos/Twitter'
+import { Linkedin } from 'styled-icons/boxicons-logos/Linkedin'
+import { Instagram } from 'styled-icons/boxicons-logos/Instagram'
+import { Pinterest } from 'styled-icons/boxicons-logos/Pinterest'
+import { Facebook } from 'styled-icons/boxicons-logos/Facebook'
+
+const Icons = {
+  Github,
+  Twitter,
+  Linkedin,
+  Instagram,
+  Pinterest,
+  Facebook
+}
+
+export default Icons
