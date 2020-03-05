cf33ba601f7340c6a452336104628fa91b7cb47d
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-03-04 22:38:47 -0300

    - add recomendação de posts  - add area de comentários

---
+++ src/components/Comments/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import ReactDisqusComments from 'react-disqus-comments'
+
+import * as S from './styled'
+
+const Comments = ({ url, title }) => {
+  const completeURL = `https://guimaraes-lucas.github.io${url}`
+
+  return (
+    <S.CommentsWrapper>
+      <S.CommentsTitle>Comentários</S.CommentsTitle>
+      <ReactDisqusComments
+        shortname="lucassguimares"
+        identifier={completeURL}
+        title={title}
+        url={completeURL}/>
+    </S.CommentsWrapper>
+  );
+}
+
+Comments.propTypes = {
+  url: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+}
+
+export default Comments
