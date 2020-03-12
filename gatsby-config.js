d4bf065fc7fe428e108dfaef846551316b6b7fce
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-03-12 16:33:17 +0000

    - App PWA funcionando!

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,18 +73,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Lucas Guimarães`,
+        short_name: `Lucas Guimarães`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-photo.jpeg`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
