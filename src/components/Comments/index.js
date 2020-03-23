0bda080129bd2d9727ac5fad273fe931dfcee506
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-03-23 09:58:59 -0300

    trocando dominio do site

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,7 +5,7 @@ import ReactDisqusComments from 'react-disqus-comments'
 import * as S from './styled'
 
 const Comments = ({ url, title }) => {
-  const completeURL = `https://guimaraes-lucas.github.io${url}`
+  const completeURL = `https://guimaraes.netlify.com${url}`
 
   return (
     <S.CommentsWrapper>
